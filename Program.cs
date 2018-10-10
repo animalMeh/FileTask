eb87a48e762b992d8e1c1561eb8ff00ddaa37d3a
D O <daria.osipova@nure.ua>
2018-10-10 21:09:32 +0300

    Need to correct linq query in search function and ask how change path to file to work with

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int [] numbers;
+            string path = @"D:\Programming\SPZ lections\lab4\FileWork\FileWork\";
+            string filename = "file.txt";
+            string text = "";
+           
+                using (StreamReader sr = new StreamReader(path + filename, System.Text.Encoding.Default))
+                {
+                    text = sr.ReadToEnd();
+                }
+
+            var umbers = text.Split(' ');
+            numbers = new int[umbers.Length];
+           
+            numbers = (from c in umbers select int.Parse(c)).ToArray();
+
+            using (StreamWriter sw = new StreamWriter(path + "out" + filename, true, System.Text.Encoding.ASCII))
+            {
+                sw.WriteLine($" Last time the most common number was referenced {GetSequence(numbers)}  times");
+            }
+     
+            
+        }
+        static int GetSequence( int [] seq)
+        {
+           var newSeq = seq.OrderByDescending(s => s).ToArray();
+           int MaxCounting = 0 , ind = 0;
+            for (int i = 0; i < newSeq.Length; i++)
+            {
+                ind++;
+                if (i == newSeq.Length - 1)
+                {
+                    if (MaxCounting < ind)
+                        MaxCounting = ind;
+                }
+                else if (newSeq[i] != newSeq[i + 1])
+                {
+                    if (MaxCounting < ind)
+                        MaxCounting = ind;
+                    ind = 0;
+                }
+            }
+           return MaxCounting;
+        }
+    }
+}
